f8628b52030512f4a517ffb12d4622ca844dd46b
ShaLin <mrsaltinecracker1@gmail.com>
2021-09-13 16:43:34 -0700

    Motor Controllers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import frc.robot.subsystems.Drivetrain;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
