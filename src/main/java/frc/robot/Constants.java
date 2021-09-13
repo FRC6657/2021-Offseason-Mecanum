e11d64af62a8ca2d1c17ce7ba633a4f316229b97
ShaLin <mrsaltinecracker1@gmail.com>
2021-09-13 16:38:18 -0700

    Cleared Repo

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,7 +9,4 @@ package frc.robot;
  * The place where variables that should never under and sercumstances ever change during runtime.
  */
 public final class Constants {
-
-    public static final double MAX_DRIVE_SPEED = 0.7; //Every drive motor power will be multiplied by this, to change the maximum output from 1 to the desired value.
-
 }
