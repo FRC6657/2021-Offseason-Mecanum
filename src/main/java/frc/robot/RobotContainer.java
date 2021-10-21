60d590b8d858eaeda24a7f08ca20750a43462e3c
scottwegley <scottwegley@gmail.com>
2021-10-21 11:54:36 -0700

    Switched Controller from xBox ro joystick

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,16 +6,18 @@ package frc.robot;
 
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
 
-  //Subsystem Inits
+  // Subsystem Inits
   private final Drivetrain m_drivetrain = new Drivetrain();
-  //HID Inits
-  private final XboxController m_controller = new XboxController(0);
+  private final Intake m_intake = new Intake();
+  // HID Inits
+  private final Joystick m_controller = new Joystick(0);
 
   public RobotContainer() {
     configureButtonBindings();
@@ -23,12 +25,11 @@ public class RobotContainer {
 
   private void configureButtonBindings() {
 
-    CommandScheduler.getInstance().setDefaultCommand(m_drivetrain, 
-      new TeleOp(
-        m_drivetrain,
-        () -> m_controller.getRawAxis(XboxController.Axis.kLeftX.value), 
-        () -> -m_controller.getRawAxis(XboxController.Axis.kLeftY.value), 
-        () -> m_controller.getRawAxis(XboxController.Axis.kRightX.value)));
+    CommandScheduler.getInstance().setDefaultCommand(m_drivetrain, new TeleOp(m_drivetrain,
+        () -> m_controller.getRawAxis(0), () -> -m_controller.getRawAxis(1), () -> m_controller.getRawAxis(2)));
+
+    final JoystickButton side = new JoystickButton(m_controller, 2);
+    side.whenHeld(new IntakePowercells(m_intake, .4));
 
   }
 
