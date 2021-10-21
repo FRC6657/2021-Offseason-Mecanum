a9ab2d7e196a8b2e0bd655c1e836df3cecd1322a
scottwegley <scottwegley@gmail.com>
2021-10-21 11:55:01 -0700

    Created a command to intake powercells

---
+++ src/main/java/frc/robot/commands/IntakePowercells.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class IntakePowercells extends CommandBase {
+
+  private final Intake intake;
+  private final double speed;
+
+  public IntakePowercells(Intake intake, double speed) {
+   
+    this.intake = intake;
+    this.speed = speed;
+
+    addRequirements(intake);
+
+  }
+
+  @Override
+  public void execute() {
+
+    intake.intake(speed);
+
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intake.intake(0);
+    
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
